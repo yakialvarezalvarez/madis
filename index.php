5ae5d9c820599f308aed29b7dcf2c01d4eacdb20
Jaquelin <jaquelin@softnovus.com>
2024-11-28 07:34:18 -0600

    segundo-commit-de-la-rama-yakii

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 	echo "esta es la rama master";
 
 	echo "estos cambios se deberian subir en la rama yakii su primer commit"
+
+	echo "este es otro cambio dentro de la misma rama segundo commit"
 ?>
